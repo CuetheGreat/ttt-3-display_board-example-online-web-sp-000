f53aede4e4e26a6c10163dae2397aafe5043aad7
Clayon Stewart <c.stewart89@gmail.com>
2021-03-02 18:38:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+ vert = "   |   |   "
+ horizontal = "-----------"
+def display_board
+  3.times do
+    puts vert
+    puts horizontal
+  end
+    
+  display_board
